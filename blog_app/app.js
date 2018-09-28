02c615a400ad6c031ed91cdea264b7626449195c
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 02:37:48 +0000

    added initialising lines in app.js

---
+++ blog_app/app.js
@@ -0,0 +1,16 @@
+var express = require("express"),
+    app = express(),
+    bodyP = require("body-parser"),
+    mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyP.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://localhost:27017/blogApp");
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Blog Sever Running-------------");
+});
