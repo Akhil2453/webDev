8cf700ef4ab7989825afdf1e2441b9a357a655ff
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 04:04:56 +0000

    created CREATE route in app.js

--- blog_app/app.js
+++ blog_app/app.js
@@ -48,6 +48,23 @@ app.get("/blogs", function(req, res){
     
 });
 
+//NEW route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE route
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if (err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog Sever Running-------------");
 });
