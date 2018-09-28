a205d730a65ff0d510d0613f31b8038df3924771
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 02:46:08 +0000

    added Blog Schema

--- blog_app/app.js
+++ blog_app/app.js
@@ -9,6 +9,17 @@ app.use(bodyP.urlencoded({extended : true}));
 
 mongoose.connect("mongodb://localhost:27017/blogApp", { useNewUrlParser: true });
 
+//ADD Schema for storing blog in data base
+var blogSchema = new mongoose.Schema ({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
