45839794cf46857a7504cb83041cdb7f20f599f4
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 00:59:28 +0000

    added server start code

--- database/movieApp/app.js
+++ database/movieApp/app.js
@@ -2,3 +2,6 @@ var express = require("express"),
     app = express(),
     request = require("request");
 
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Sever has Started!!!!") ;
+});
