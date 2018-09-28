4893b8e838abccd2c607eed89d93d9eeac317fd7
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 01:08:33 +0000

    changed folder location for movieApp

---
+++ movieApp/app.js
@@ -0,0 +1,16 @@
+var express = require("express"),
+    app = express(),
+    request = require("requests");
+    
+app.set("view engine", "ejs");
+
+//Home page(Search page in this case)
+app.get("/", function(req,res){
+    res.redirect("search");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Sever has Started!!!!") ;
+});
