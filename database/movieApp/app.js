ba65fbf9499bce6cab99f95a1b9737cca30dd35e
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 01:03:00 +0000

    set the view engine as ejs

--- database/movieApp/app.js
+++ database/movieApp/app.js
@@ -1,6 +1,11 @@
 var express = require("express"),
     app = express(),
     request = require("requests");
+    
+app.set("view engine", "ejs");
+
+//Home page(Search page in this case)
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Sever has Started!!!!") ;
