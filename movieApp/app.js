e18007b0ac727a787f42d57ccb2164ab1437ec0b
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 01:16:12 +0000

    Added result requests

--- movieApp/app.js
+++ movieApp/app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app = express(),
-    request = require("requests");
+    request = require("request");
     
 app.set("view engine", "ejs");
 
@@ -9,7 +9,18 @@ app.get("/", function(req,res){
     res.redirect("search");
 });
 
-
+//Display page for search results
+app.get("/results", function(req,res){
+   var query = res.query.search;
+   var url = "http://omdbapi.com/s?=" + query + "&apikey=thewdb";
+   
+   request(url, function(error, response, body){
+       if(!error && response.statusCode == 200){
+           var data = JSON.parse(body);
+           res.render("display", {data : data});
+       }
+   })
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Sever has Started!!!!") ;
