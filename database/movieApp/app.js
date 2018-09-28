ff1a815b281cf3481a44b4779be18dee47cb15d4
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 01:00:40 +0000

    resolved error

--- database/movieApp/app.js
+++ database/movieApp/app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app = express(),
-    request = require("request");
+    request = require("requests");
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Sever has Started!!!!") ;
