db049b00c7c65af9528290899b6774657f6a15d9
Akhil Menon <akhil.menon24@gmail.com>
2018-09-28 02:58:11 +0000

    created blog entry in database

--- blog_app/app.js
+++ blog_app/app.js
@@ -23,7 +23,12 @@ var blogSchema = new mongoose.Schema ({
 //CREATE Databse model
 var Blog = mongoose.model("Blog", blogSchema);
 
-
+//Temp data in Blog DB
+Blog.create({
+    title: "Test Post",
+    image: "https://images.unsplash.com/photo-1538000416583-35c50e541862?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a860997bc839a5afc470a51cb5fa9ac1&auto=format&fit=crop&w=500&q=60",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog Sever Running-------------");
